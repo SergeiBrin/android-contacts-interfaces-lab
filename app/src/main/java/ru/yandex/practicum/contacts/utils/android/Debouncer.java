3cc1633065d03e70f68c200d85d24bb76fdc7cf4
Sergei <starodumovtmt@yandex.ru>
2024-03-01 03:42:34 +0500

    feat: добавить интерфейс

--- app/src/main/java/ru/yandex/practicum/contacts/utils/android/Debouncer.java
+++ app/src/main/java/ru/yandex/practicum/contacts/utils/android/Debouncer.java
@@ -13,17 +13,17 @@ public class Debouncer {
     private static final int MESSAGE_ID = 1;
     private static final int DELAY = 500;
 
-    private final MainViewModel viewModel;
+    private final OnDebounceListener listener;
 
-    public Debouncer(MainViewModel viewModel) {
-        this.viewModel = viewModel;
+    public Debouncer(OnDebounceListener listener) {
+        this.listener = listener;
     }
 
     private final Handler handler = new Handler(Looper.getMainLooper()) {
         @Override
         public void handleMessage(@NonNull Message message) {
             if (message.what == MESSAGE_ID) {
-                doOnDebounce();
+                listener.doOnDebounce();
                 return;
             }
             super.handleMessage(message);
@@ -35,10 +35,5 @@ public class Debouncer {
         final Message message = Message.obtain(handler, MESSAGE_ID, value);
         handler.removeMessages(MESSAGE_ID);
         handler.sendMessageDelayed(message, DELAY);
-    }
-
-    // выполнить действие по прошествии 500мс, если нового события в течении 500мс не было отправлено
-    private void doOnDebounce() {
-        viewModel.search();
     }
 }
